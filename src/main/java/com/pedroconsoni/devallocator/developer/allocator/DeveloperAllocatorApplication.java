c7c8641e7a1835aa52765c959fdc046effea2cb7
Pedro Consoni <pedro.consoni.ph@gmail.com>
2025-08-05 10:39:02 -0300

    first commit

---
+++ src/main/java/com/pedroconsoni/devallocator/developer/allocator/DeveloperAllocatorApplication.java
@@ -0,0 +1,13 @@
+package com.pedroconsoni.devallocator.developer.allocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperAllocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperAllocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroconsoni/devallocator/developer/allocator/DeveloperAllocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroconsoni.devallocator.developer.allocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperAllocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

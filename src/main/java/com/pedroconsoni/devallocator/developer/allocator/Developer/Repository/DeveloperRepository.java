e2128ec00eb7e0a863e916fd5a011f2aa40f61c5
Pedro Consoni <pedro.consoni.ph@gmail.com>
2025-08-07 11:45:49 -0300

    build: Connect the H2 database to my application SPRING

--- src/main/java/com/pedroconsoni/devallocator/developer/allocator/Developer/Repository/DeveloperRepository.java
+++ src/main/java/com/pedroconsoni/devallocator/developer/allocator/Developer/Repository/DeveloperRepository.java
@@ -1,4 +1,9 @@
 package com.pedroconsoni.devallocator.developer.allocator.Developer.Repository;
 
-public interface DeveloperRepository {
+import com.pedroconsoni.devallocator.developer.allocator.Developer.Model.DeveloperModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperRepository extends JpaRepository< DeveloperModel, Long > {
+
+
 }

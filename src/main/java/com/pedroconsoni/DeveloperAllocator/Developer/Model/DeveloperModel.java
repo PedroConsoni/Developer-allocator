ea14e550559ac85c08d102a0f6d1da2c3ec8fec6
Pedro Consoni <pedro.consoni.ph@gmail.com>
2025-08-13 12:17:30 -0300

    fix: Set the email column as UNIQUE

--- src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/Model/DeveloperModel.java
+++ src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/Model/DeveloperModel.java
@@ -2,16 +2,22 @@ package com.pedroconsoni.DeveloperAllocator.Developer.Model;
 
 import com.pedroconsoni.DeveloperAllocator.Project.Model.ProjectModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_developer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class DeveloperModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Column(name = "email")
+    @Column(unique = true)
     private String email;
 
     @Column(name = "name")
@@ -25,36 +31,4 @@ public class DeveloperModel {
     @JoinColumn(name = "project_id")
     private ProjectModel project;
 
-    public DeveloperModel() {
-    }
-
-    public DeveloperModel(String email, String name, int age) {
-        this.email = email;
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }

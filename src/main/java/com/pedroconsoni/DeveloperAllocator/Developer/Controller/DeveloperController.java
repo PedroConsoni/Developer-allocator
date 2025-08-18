21062ff3563dab4d1ca6f199d71236f9ecf3c359
Pedro Consoni <pedro.consoni.ph@gmail.com>
2025-08-18 12:02:25 -0300

    feat: Updates the controller with the data mock

--- src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/Controller/DeveloperController.java
+++ src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/Controller/DeveloperController.java
@@ -1,8 +1,6 @@
 package com.pedroconsoni.DeveloperAllocator.Developer.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -14,5 +12,35 @@ public class DeveloperController {
     public String hello() {
         return "Hello";
     }
+
+    // Add developer
+    @PostMapping("/add")
+    public String addDeveloper() {
+        return "Developer add";
+    }
+
+    // List all registered developers
+    @GetMapping("/all")
+    public String showAllDevelopers() {
+        return "Show all developers";
+    }
+
+    // List registered developers by ID
+    @GetMapping("/allid")
+    public String showDevelopersByID() {
+        return "show developers by id";
+    }
+
+    // Change developer data
+    @PutMapping("/changeid")
+    public String changeDevelopersByID() {
+        return "Change developers by id";
+    }
+
+    // Delete developer
+    @DeleteMapping("/deleteid")
+    public String deleteByID() {
+        return "Developer delete by ID";
+}
 
 }

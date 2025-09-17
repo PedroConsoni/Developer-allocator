4e52a74788bd6a1296a7fb48d9b82a9367e782a5
Pedro Consoni <pedro.consoni.ph@gmail.com>
2025-09-17 12:27:46 -0300

    Add Swagger to my API to document my routes in the Controller

--- src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/DeveloperController.java
+++ src/main/java/com/pedroconsoni/DeveloperAllocator/Developer/DeveloperController.java
@@ -1,6 +1,8 @@
 package com.pedroconsoni.DeveloperAllocator.Developer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,13 @@ public class DeveloperController {
 
     // Create developer
     @PostMapping("/create")
+    @Operation(summary = "Developers creation", description = "This route creates the developers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Resource created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "409", description = "Existing resource"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<String> createDeveloper(@RequestBody DeveloperDTO developerDTO) {
         DeveloperDTO developer = developerService.createDeveloper(developerDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -26,6 +35,11 @@ public class DeveloperController {
 
     // List all registered developers
     @GetMapping("/list")
+    @Operation(summary = "List all developers", description = "This route lists all developers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<List<DeveloperDTO>> listDeveloper() {
         List<DeveloperDTO> developers = developerService.listDeveloper();
         return ResponseEntity.ok(developers);
@@ -33,6 +47,13 @@ public class DeveloperController {
 
     // List registered developers by ID
     @GetMapping("/list/{id}")
+    @Operation(summary = "List developers by ID", description = "This route lists developers by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<?> listDeveloperByID(@PathVariable Long id) {
         DeveloperDTO developer = developerService.listDeveloperByID(id);
 
@@ -46,6 +67,14 @@ public class DeveloperController {
 
     // Update developer data
     @PutMapping("/update/{id}")
+    @Operation(summary = "Updates developers by ID", description = "This route updates developers by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "204", description = "Update successful but no response body"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<?> updateDeveloperByID(@PathVariable Long id, @RequestBody DeveloperDTO updatedDeveloper) {
         DeveloperDTO developer = developerService.updateDeveloperByID(id, updatedDeveloper);
 
@@ -59,6 +88,14 @@ public class DeveloperController {
 
     // Delete developer
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete developers by ID", description = "This route deletes developers by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Resource deleted successfully"),
+            @ApiResponse(responseCode = "204", description = "Update successful but no response body"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<String> deleteDeveloperByID(@PathVariable Long id) {
         if (developerService.listDeveloperByID(id) != null) {
             developerService.deleteDeveloperByID(id);
--- src/main/java/com/pedroconsoni/DeveloperAllocator/Project/ProjectController.java
+++ src/main/java/com/pedroconsoni/DeveloperAllocator/Project/ProjectController.java
@@ -2,6 +2,9 @@ package com.pedroconsoni.DeveloperAllocator.Project;
 import com.pedroconsoni.DeveloperAllocator.Developer.DeveloperController;
 import com.pedroconsoni.DeveloperAllocator.Developer.DeveloperDTO;
 import com.pedroconsoni.DeveloperAllocator.Developer.DeveloperModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,13 @@ public class ProjectController {
 
     // Create project
     @PostMapping("/create")
+    @Operation(summary = "Projects creation", description = "This route creates the projects")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Resource created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "409", description = "Existing resource"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<String> createProject(@RequestBody ProjectDTO projectDTO) {
         ProjectDTO project = projectService.createProject(projectDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -29,6 +39,11 @@ public class ProjectController {
 
     // List all registered projects
     @GetMapping("/list")
+    @Operation(summary = "List all projects", description = "This route lists all projects")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<List<ProjectDTO>> listProject() {
         List<ProjectDTO> projects = projectService.listProject();
         return ResponseEntity.ok(projects);
@@ -36,6 +51,13 @@ public class ProjectController {
 
     // List registered projects by ID
     @GetMapping("/list/{id}")
+    @Operation(summary = "List projects by ID", description = "This route lists projects by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<?> listProjectsByID(@PathVariable Long id) {
         ProjectDTO project = projectService.listProjectByID(id);
 
@@ -49,6 +71,14 @@ public class ProjectController {
 
     // Update project data
     @PutMapping("/update/{id}")
+    @Operation(summary = "Updates projects by ID", description = "This route updates projects by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Resource found and returned successfully"),
+            @ApiResponse(responseCode = "204", description = "Update successful but no response body"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<?> updateProjectByID(@PathVariable Long id, @RequestBody ProjectDTO updatedProject) {
         ProjectDTO project = projectService.updateProjectByID(id, updatedProject);
 
@@ -62,6 +92,14 @@ public class ProjectController {
 
     // Delete project
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete projects by ID", description = "This route deletes projects by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Resource deleted successfully"),
+            @ApiResponse(responseCode = "204", description = "Update successful but no response body"),
+            @ApiResponse(responseCode = "400", description = "Invalid data sent"),
+            @ApiResponse(responseCode = "404", description = "Reported resource not found"),
+            @ApiResponse(responseCode = "500", description = "Something unexpected happened on the server")
+    })
     public ResponseEntity<String> deleteProjectByID(@PathVariable Long id) {
         if (projectService.listProjectByID(id) != null) {
             projectService.deleteProjectByID(id);
